66da1ca588fe857810cce7c90771162b9019f1c9
yuto-taira-pixel <yuto-taira@gr.paramix.co.jp>
2026-04-16 16:10:16 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,17 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// 車クラスのインスタンスを作成
+        Car_Chapter15 car = new Car_Chapter15();
+
+        // ギアを3に変更する
+        car.changeGear(3);
+
+        // 走行メソッドを実行する
+        car.run();
+	}
+
+}
